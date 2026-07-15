2c6ff192900487f75ec804979ee6885810d540d8
rahuln577 <rahulrahuln2001@gmail.com>
2026-07-15 20:17:04 +0530

    day 45

---
+++ Week4/DisjointSet.cpp
@@ -0,0 +1,39 @@
+class DisjointSet
+{
+    private:
+        vector<int> parent;
+        vector<int> size;
+    public:
+        DisjointSet(int n)
+        {
+            parent.resize(n);
+            size.resize(n);
+            for(int i=0;i<n;i++)
+            {
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+        int findUParent(int n)
+        {
+            if(parent[n] == n)return n;
+            return (parent[n] = findUParent(parent[n]));
+        }
+        void unionn(int u,int v)
+        {
+            int pu = findUParent(u);
+            int pv = findUParent(v);
+            if(pu == pv)return;
+            
+            if(size[pu]>size[pv])
+            {
+                parent[pv] = pu;
+                size[pu]+=size[pv];
+            }
+            else if(size[pv]>=size[pu])
+            {
+                parent[pu] = pv;
+                size[pv]+=size[pu];
+            }
+        }
+};
